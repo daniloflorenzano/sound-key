8df54d558648343709634cd99b6ce3c5fe4e553e
Dandeiz <daniloflorenzano1@outlook.com>
2021-09-08 22:53:23 -0300

    added upload function

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog, ipcMain } = require('electron');
+const os = require('os');
 
 const path = require('path');
 const url = require('url');
@@ -21,3 +22,9 @@ app.on('ready', () => {
     win.loadURL(`file://${__dirname}/app/view/index.html`);
 })
 
+ipcMain.on('open-file-dialog', (e) => {
+    const properties = os.platform() === 'linux' || os.platform() === 'win32' ? ['openFile'] : ['openFile', 'openDirectory'];
+    dialog.showOpenDialog({ properties }).then((data) => {
+        if (data.filePaths.length > 0) { e.sender.send('selected-file', data.filePaths[0]); }
+    })
+})
