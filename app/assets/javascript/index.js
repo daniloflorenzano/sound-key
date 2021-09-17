f8b62f2c1e115f9f1481e72a7b13151e45e5ffe0
Dandeiz <daniloflorenzano1@outlook.com>
2021-09-16 21:38:09 -0300

    validate form working

--- app/assets/javascript/index.js
+++ app/assets/javascript/index.js
@@ -37,42 +37,9 @@ function recordKeyPressed() {
   iohook.start();
 }
 
-async function validateForm() {
-  const databaseContent = await Macros.findAll({
-    attributes: ['name', 'key', 'file_path']
-  });
-
-  const keybindsString = JSON.stringify(databaseContent);
-  const keybindsObj = JSON.parse(keybindsString)
-  console.log('Keybindss:', keybindsObj);
-
-  let newMacro = {
-    name: nameInput.value,
-    key: keyPressed[1],
-    keyCode: keyPressed[0],
-    file_path: filePath,
-  };
-
-  keybindsObj.map((keybind) => {
-    if (keybind.name == newMacro.name) {
-      console.log(':::: Name already in use ::::');
-      return false;
-    } else if (keybind.key == newMacro.key) {
-      console.log(':::: Key already in use ::::');
-      return false;
-    } else if (keybind.file_path == newMacro.file_path) {
-      console.log(':::: Audio already in use ::::');
-      return false;
-    } else if (!filePath.endsWith('.mp3') && !filePath.endsWith('.wav')) {
-      console.log(':::: Please use a MP3 or WAV file ::::')
-      return false;
-    } else { createMacro(newMacro) };
-  })
-}
-
-function createMacro(newMacro) {
-
-  MacrosController.create(newMacro);
-  console.log(newMacro);
+function createMacro(data) {
+  
+  MacrosController.create(data);
+  console.log(data);
   console.log('::::: Keybind Created :::::');
 }
---
+++ app/assets/javascript/validate.js
@@ -0,0 +1,60 @@
+const validateForm = async () => {
+    const databaseContent = await Macros.findAll({
+        attributes: ['name', 'key', 'file_path']
+    });
+
+    const keybindsString = JSON.stringify(databaseContent);
+    const keybindsObj = JSON.parse(keybindsString)
+    console.log('Keybinds:', keybindsObj);
+
+    let newMacro = {
+        name: nameInput.value,
+        key: [...keyPressed].pop(),
+        keyCode: keyPressed[0],
+        file_path: filePath,
+    };
+
+    for (let i = 0; i < keybindsObj.length; i++) {
+        data = keybindsObj[i];
+    }
+
+    if(
+        validateName(data, newMacro) == true &&
+        validateKey(data, newMacro) == true &&
+        validateAudio(data, newMacro) == true &&
+        validateFileType() == true
+    ) createMacro(newMacro);
+}
+
+function validateName(data, obj) {
+    if (data.name == obj.name || obj.name == null) {
+        window.alert('Name is missing or is already in use');
+        console.log(':::: Name is missing or is already in use ::::');
+        return false;
+    } else return true;
+}
+
+function validateKey(data, obj) {
+    if (data.key == obj.key || obj.key == null) {
+        window.alert('Key is missing or is already in use');
+        console.log(':::: Key is missing or is already in use ::::');
+        return false;
+    } else return true;
+}
+
+function validateAudio(data, obj) {
+    if (data.file_path == obj.file_path || obj.file_path == null) {
+        window.alert('Audio is missing or is already in use');
+        console.log(':::: Audio is missing or is already in use ::::');
+        return false;
+    } else return true;
+}
+
+function validateFileType() {
+    if (!filePath.endsWith('.mp3') && !filePath.endsWith('.wav')) {
+        window.alert('Please use a MP3 or WAV file');
+        console.log(':::: Please use a MP3 or WAV file ::::')
+        return;
+    } else return true;
+}
+
